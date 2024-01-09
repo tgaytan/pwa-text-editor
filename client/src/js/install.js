b36fb8d0d63f93252c9fd16b008d593030f49c16
tgaytan <tgaytan14@utexas.edu>
2024-01-08 18:40:35 -0600

    installation seems to work fine and app seems to run fine offline

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,36 @@
 const butInstall = document.getElementById('buttonInstall');
+let installPrompt = null
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault()
+    console.log(event)
+    installPrompt = event
+
+    // butInstall.addEventListener('click', () => {
+    //     event.prompt()
+    //     butInstall.setAttribute('disabled', true)
+    //     butInstall.textContent = 'Installed!'
+    // })
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async (event) => {
+    if (!installPrompt) {
+        butInstall.textContent = 'Installed'
+        return
+    }
+    const result = await installPrompt.prompt()
+    disableInAppInstallPrompt()
+});
+
+function disableInAppInstallPrompt() {
+    installPrompt = null
+    butInstall.textContent = 'Installed'
+}
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    disableInAppInstallPrompt()
+});
